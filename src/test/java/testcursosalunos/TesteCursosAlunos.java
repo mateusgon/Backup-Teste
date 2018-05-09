c11700ed1f1b0c2ddb42cfd0a565849df9d37713
Mateus G <Mateus G@MateusG>
2018-05-09 18:19:46 -0300

    Adicionando Alunos aos Cursos

--- src/test/java/testcursosalunos/TesteCursosAlunos.java
+++ src/test/java/testcursosalunos/TesteCursosAlunos.java
@@ -1,5 +1,39 @@
 package testcursosalunos;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import testcursos.TesteCursosAux;
+
 public class TesteCursosAlunos {
+        
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void configura()
+    {
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Mateus G\\Desktop\\geckodriver.exe");
+        driver = (WebDriver) new FirefoxDriver();
+        driver.get("https://atlantis.isti.cnr.it:5000/");
+    }
     
+    @Test
+    public void testLoginCorrectTeacher()
+    {
+        TesteCursosAlunosAux pag = PageFactory.initElements(driver, TesteCursosAlunosAux.class);
+        pag.loga();
+        pag.preencheDados("teacher@gmail.com", "Pass12345");
+        pag.acessaLoginModal();
+        // >>> Adiciona aluno, caso ele não esteja no curso <<< pag.adicionarAluno();
+        // >>> Não deverá adicionar alunos, adicionar aluno não cadastrado <<< pag.adicionarAlunoNaoCadastrado();
+        // >>> Não deverá adicionar aluno, pois já está na turma <<< pag.adicionarAlunoTurmaExistente();
+        // >>> Adiciona aluno em grupo, dois alunos não existentes. Caso exista um ou os dois não existam, 
+        // ele adicionará somente o que não existe ou nenhum <<< pag.adicionarAlunoMultiplo();
+        // >>> Não deverá ser possível adicionar os alunos nos casos a seguir 
+        // pag.adicionarAlunoVazio();
+        // pag.adicionarAlunoComEmailIncorreto();
+        // pag.adicionarAlunoSemEmail();
+    }
 }
--- src/test/java/testcursosalunos/TesteCursosAlunosAux.java
+++ src/test/java/testcursosalunos/TesteCursosAlunosAux.java
@@ -1,5 +1,163 @@
 package testcursosalunos;
 
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import testcursos.TesteCursosAux;
+
 public class TesteCursosAlunosAux {
     
+    @FindBy(id = "input-attender-multiple")
+    private WebElement textoAlunos;
+    
+    @FindBy(id =  "input-attender-separator")
+    private WebElement textoSeparador;
+    
+    @FindBy(id = "put-modal-btn")
+    private WebElement botaoConfirmaAluno;
+    
+    @FindBy(id = "input-attender-simple")
+    private WebElement emailAluno;
+    
+    @FindBy(id = "add-attenders-icon")
+    private WebElement adicionarAluno;
+    
+    @FindBy(id = "md-tab-label-0-4")
+    private WebElement abaEdicao;
+    
+    @FindBy(id = "md-tab-label-1-1")
+    private WebElement abaMultiplo;
+    
+    @FindBy(css ="div[class='col l6 m7 s7 valign course-title']")
+    List<WebElement> listCourses;
+    
+    @FindBy(id = "email")
+    private WebElement email;
+
+    @FindBy(id = "password")
+    private WebElement password;
+
+    @FindBy(id = "signUpButton")
+    private WebElement botao;
+
+    @FindBy(id = "log-in-btn")
+    WebElement botaoLogin;
+
+    private WebDriver driver;
+    private WebDriver driver2;
+    private WebDriver driver3;
+    
+    public TesteCursosAlunosAux(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public TesteCursosAlunosAux preencheDados(String nome, String sobrenome) {
+        this.email.sendKeys(nome);
+        this.password.sendKeys(sobrenome);
+        PageFactory.initElements(driver, this);
+        return this;
+    }
+
+    public void loga() {
+        driver.findElement(By.linkText("Log in")).click();
+    }
+
+    public void acessaLoginModal() {
+        botaoLogin.click();
+    }
+    
+    public void adicionarAluno()
+    {
+        driver2 = (WebDriver) new FirefoxDriver();
+        driver2.close();
+        listCourses.get(0).click();
+        driver3 = (WebDriver) new FirefoxDriver();
+        driver3.close();
+        abaEdicao.click();
+        adicionarAluno.click();
+        emailAluno.sendKeys("student2@gmail.com");
+        botaoConfirmaAluno.click();
+    } 
+        public void adicionarAlunoNaoCadastrado()
+    {
+        driver2 = (WebDriver) new FirefoxDriver();
+        driver2.close();
+        listCourses.get(0).click();
+        driver3 = (WebDriver) new FirefoxDriver();
+        driver3.close();
+        abaEdicao.click();
+        adicionarAluno.click();
+        emailAluno.sendKeys("teste1111@teste.com");
+        botaoConfirmaAluno.click();
+    }  
+        
+    public void adicionarAlunoTurmaExistente()
+    {
+        driver2 = (WebDriver) new FirefoxDriver();
+        driver2.close();
+        listCourses.get(0).click();
+        driver3 = (WebDriver) new FirefoxDriver();
+        driver3.close();
+        abaEdicao.click();
+        adicionarAluno.click();
+        emailAluno.sendKeys("student1@gmail.com");
+        botaoConfirmaAluno.click();
+    }
+    
+    public void adicionarAlunoMultiplo()
+    {
+        driver2 = (WebDriver) new FirefoxDriver();
+        driver2.close();
+        listCourses.get(0).click();
+        driver3 = (WebDriver) new FirefoxDriver();
+        driver3.close();
+        abaEdicao.click();
+        adicionarAluno.click();
+        abaMultiplo.click();
+        textoSeparador.sendKeys(",");
+        textoAlunos.sendKeys("student1@gmail.com,student2@gmail.com");
+        botaoConfirmaAluno.click();    
+    }
+    
+    public void adicionarAlunoVazio()
+    {
+        driver2 = (WebDriver) new FirefoxDriver();
+        driver2.close();
+        listCourses.get(0).click();
+        driver3 = (WebDriver) new FirefoxDriver();
+        driver3.close();
+        abaEdicao.click();
+        adicionarAluno.click();
+        emailAluno.sendKeys("");
+        botaoConfirmaAluno.click();
+    }
+        public void adicionarAlunoComEmailIncorreto()
+    {
+        driver2 = (WebDriver) new FirefoxDriver();
+        driver2.close();
+        listCourses.get(0).click();
+        driver3 = (WebDriver) new FirefoxDriver();
+        driver3.close();
+        abaEdicao.click();
+        adicionarAluno.click();
+        emailAluno.sendKeys("teste@teste");
+        botaoConfirmaAluno.click();
+    }
+    
+        public void adicionarAlunoSemEmail()
+    {
+        driver2 = (WebDriver) new FirefoxDriver();
+        driver2.close();
+        listCourses.get(0).click();
+        driver3 = (WebDriver) new FirefoxDriver();
+        driver3.close();
+        abaEdicao.click();
+        adicionarAluno.click();
+        emailAluno.sendKeys("teste");
+        botaoConfirmaAluno.click();
+    }
 }
